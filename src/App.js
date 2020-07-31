9775f396ee26ad7a415b2260a3a990b69c8fc201
iamtamtamiam <vutam_a@hotmail.com>
2020-07-31 15:11:51 -0500

    install pacakges, import initial components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { render } from '@testing-library/react';
+//import { render } from '@testing-library/react';
+
+
+
 
 // function App() {
 //   return (
@@ -33,21 +36,11 @@ class App extends React.Component{
   }
   render(){
     return (
-        <div className="App">
-          <header className="App-header">
+        <div>
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-              Edit <code>src/App.js</code> and save to reload.
+              inside scr/app.js
             </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
         </div>
       );
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import { createStore, applyMiddleware, compose, combineReducers} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import usersReducer from './reducers/user'
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+const reducer = combineReducers({
+  users: usersReducer
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+ReactDOM.render (
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
-);
+
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
