9fc158832dbc7e96d1ea6f0516903fbb6e4270f2
iamtamtamiam <vutam_a@hotmail.com>
2020-08-08 23:49:19 -0500

    start listingAttributes for a Listing, not working

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import {connect} from 'react-redux';
+//import {connect} from 'react-redux';
 //import {logout} from '../actions/userActions'
 
 const Listing = (props) => {
+    console.log(props)
 
+    // const listingAttributes = props.listing.attributes.forEach(attribute => {
+    //     return (
+    //         [attribute.key, attribute.value]
+    //     )
+    // })
+
+    // const listingAttributes = () => {
+    //     console.log(props.listing.attributes)
+    //     for(const attribute in props.listing.attributes) {
+    //         return (`${attribute}: ${props.listing.attributes[attribute]}`)
+    //     }
+    // 
+    // }
+
+
+    const listingAttributes = () => {
+        let attributes = props.listing.attributes
+        console.log(attributes)
+        for (const [key, value] of Object.entries(attributes)) {
+           return (`${key}: ${value}`);
+          }
     
+    }
 
 
     return (
         <div>
             this will be an indiviudal listing
+            <li>
+            {listingAttributes()}
+            </li>
+
         </div>
 
     )
@@ -17,4 +44,4 @@ const Listing = (props) => {
 
 }
 
-export default connect(null)(Listing)
+export default Listing
