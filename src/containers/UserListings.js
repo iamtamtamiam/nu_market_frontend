5643590de933e7aa942d12c801eb022bc8341cc4
iamtamtamiam <vutam_a@hotmail.com>
2020-10-19 11:37:06 -0500

    remove comments and console.logs

--- src/containers/UserListings.js
+++ src/containers/UserListings.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+
 import {Link} from 'react-router-dom'
 
 import UserListingCard from '../components/UserListingCard'
@@ -13,16 +13,14 @@ class UserListings extends Component {
     render () {
 
         const {currentUser, AllListings} = this.props
-        //const currentUser2 = this.props.currentUser
-
-        //NEED TO TAKE OUT LINK TO NEW BC OF BUTTON
+        
 
         const linkToNew = <Link exact="true" to={`/listings/new`}>Add a New Listing</Link>
 
 
         {while (currentUser === "") { return (<div>You have no listings... {linkToNew}</div>)}
 
-        const sellerListings = currentUser.attributes.seller_listings
+        //const sellerListings = currentUser.attributes.seller_listings
         const userListings = AllListings.filter(listing => listing.relationships.seller.data.id === currentUser.id)
       
 
@@ -31,7 +29,7 @@ class UserListings extends Component {
             <div>
                 <Jumbotron 
                     style={ {width: '100%', objectFit: 'cover', textAlign: 'center', backgroundColor: '#5982ab'} }>
-                        <img src="https://i.ibb.co/5W97v0s/output-onlinepngtools.png"
+                        <img src="https://i.ibb.co/5W97v0s/output-onlinepngtools.png" alt="nu_logo"
                             style={ {width: '20%', height: '20%'}}
                         ></img>
                         
@@ -45,12 +43,6 @@ class UserListings extends Component {
                 </Link>
                
              
-                {console.log(this.props)}
-                {console.log(currentUser)}
-                {console.log(currentUser.attributes.seller_listings)}
-                {/*console.log(sellerListings)*/}\
-                {console.log("this is sellerl", sellerListings)}
-                {console.log("this is user l", userListings)}
                 <Container>
                     <CardColumns>
                         {userListings.map(item =>
@@ -68,7 +60,7 @@ class UserListings extends Component {
                     </CardColumns>
                 </Container>
 
-                End of Listings
+               
                 <Link exact="true" to={`/listings/new`}>
                     <Button variant="outline-success" size="lg" block >
                         Add a New Listing
@@ -79,27 +71,14 @@ class UserListings extends Component {
 
         )
 
-
-
-
     
         }
 
-     
-        
-
     }
-
-
-
 
 }
 
 
-// <UserListingCard
-// key={item.id}
-// listing={item}
-// />
 
 const mapStateToProps = state => {
     return {
