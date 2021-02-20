869b20522ba7c5d0a63cfe84ad53ab424ba362f3
iamtamtamiam <vutam_a@hotmail.com>
2021-02-20 17:24:51 -0600

    add renderZipForm()

--- src/components/ZipCodeForm.js
+++ src/components/ZipCodeForm.js
@@ -4,7 +4,32 @@ import Form from 'react-bootstrap/Form';
 
 class ZipCodeForm extends React.Component {
 
+  state = {
+    formChecked: false
+  }
 
+  handleChange = event => {
+    console.log("in handle change for switch")
+    //console.log(event.target.checked)
+      this.setState({
+        formChecked: event.target.checked
+      })
+    
+  }
+
+  renderZipForm = () => {
+    if (this.state.formChecked === true){
+      return(
+       //either move form at bottom or take away form here
+        <form>
+          <label>Enter ZipCode: </label>
+            <input type="text"   name="zipcode" placeholder="function not complete"/>
+        </form>
+       
+      )
+    }
+
+  }
 
 
     render(){
@@ -14,7 +39,11 @@ class ZipCodeForm extends React.Component {
             type="switch"
             id="custom-switch"
             label="Check this switch"
+            onChange={this.handleChange}
           />
+
+          {this.renderZipForm()}
+
         </Form>
         )
     }
